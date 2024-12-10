28a23760e8e58355476585727529d539d2d37612
Adedeji <aadeleke91618330@gmail.com>
2024-12-10 08:08:27 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Intended Audience :: Developers"
-    "Operating System :: OS Independent",
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'License :: OSI Approved :: MIT License',
 ]
 
 setup(
     name="pylytic",
     version="0.1.0",
     description="A lightweight library for evaluating mathematical expressions and functions",
-    long_description=open("README.md").read(),
-    long_description_context_type="text/markdown",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author="Adeleke Adedeji",
     author_email="aadeleke91618330@gmail.com",
     license="MIT",
@@ -22,5 +26,5 @@ setup(
     include_package_data=True,
     classifiers=classifiers,
     python_requires=">=3.8",
-    install_requires=[],
+    install_requires=[]
 )
